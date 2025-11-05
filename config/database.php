64b827c9c0ff7060b2028def6e7534c90aee7b8f
Reginaldo Aparecido de Souza <106198394+reginaldosouzasouza@users.noreply.github.com>
2025-11-05 19:13:04 -0300

    Ajusta conexão MySQL com SSL para Aiven

--- config/database.php
+++ config/database.php
@@ -42,21 +42,31 @@ return [
             'synchronous' => null,
         ],
 
-       
-
-            'mysql' => [ // Conexão padrão para o banco gas
+       'mysql' => [
             'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'gas'), // Aponta para o banco 'gas'
-            'username' => env('DB_USERNAME', 'root'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
+            'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
+
+            'options' => array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA', '/etc/ssl/certs/ca-certificates.crt'),
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false, // 👈 garante compatibilidade
+            ]),
         ],
+
+
+           
+        
 
 
    
