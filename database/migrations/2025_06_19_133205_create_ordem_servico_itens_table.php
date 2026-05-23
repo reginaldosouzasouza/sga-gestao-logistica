cf522fe42b0055faf50dbb67dab7a55f630bfb79
REGINALDO SOUZA <reginaldo.aparecido.souzas@gmail.com>
2026-05-23 18:02:02 -0300

    fix formas_de_pagamento reference

--- database/migrations/2025_06_19_133205_create_ordem_servico_itens_table.php
+++ database/migrations/2025_06_19_133205_create_ordem_servico_itens_table.php
@@ -1,5 +1,4 @@
 <?php
-    
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -10,22 +9,18 @@ return new class extends Migration {
     {
         Schema::create('ordem_servico_itens', function (Blueprint $table) {
             $table->id();
-
             $table->unsignedBigInteger('ordem_servico_id');
             $table->unsignedBigInteger('produto_id');
             $table->integer('quantidade');
             $table->decimal('valor_unitario', 10, 2);
             $table->decimal('valor_total', 10, 2);
-
             $table->unsignedBigInteger('forma_pagamento_id')->nullable();
             $table->unsignedBigInteger('prazo_id')->nullable();
-
             $table->timestamps();
 
-            // Foreign keys
             $table->foreign('ordem_servico_id')->references('id')->on('ordens_servico')->onDelete('cascade');
             $table->foreign('produto_id')->references('id')->on('produtos')->onDelete('restrict');
-            $table->foreign('forma_pagamento_id')->references('id')->on('formas_pagamento')->onDelete('set null');
+            $table->foreign('forma_pagamento_id')->references('id')->on('formas_de_pagamento')->onDelete('set null');
             $table->foreign('prazo_id')->references('id')->on('prazos')->onDelete('set null');
         });
     }
