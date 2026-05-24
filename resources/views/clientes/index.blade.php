634cb677b8ffe9718ee1ca40c54a9f470428e6c3
REGINALDO SOUZA <reginaldo.aparecido.souzas@gmail.com>
2026-05-24 12:28:12 -0300

    ajuste responsivo tela de clientes

--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -10,7 +10,7 @@
 
 <div class="clientes-page">
 
-    <div class="container">
+    <div class="container container-pesquisa">
         <input 
             type="text" 
             id="search" 
@@ -19,70 +19,72 @@
         >
     </div>
 
-    <div class="total-clientes" style="margin-bottom: 10px;">
+    <div class="total-clientes">
         <strong>Total de Clientes: {{ $totalClientes }}</strong>
     </div>
 
-    <div class="container">
+    <div class="container container-listagem">
 
-        <a href="{{ route('clientes.create') }}" class="btn btn-adicionar">
-            Cadastrar Cliente
-        </a>
-        
-        <h1 class="title">CLIENTES</h1>
+        <div class="topo-listagem">
+            <a href="{{ route('clientes.create') }}" class="btn btn-adicionar">
+                Cadastrar Cliente
+            </a>
+            
+            <h1 class="title">CLIENTES</h1>
+        </div>
 
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>Telefone</th>
-                    <th>CPF</th>
-                    <th>Nome</th>                  
-                    <th>Endereço</th>
-                    <th>Número</th>
-                    <th>Bairro</th>
-                    <th>Cidade</th>
-                  
-                    <th>Ações</th>
-                </tr>
-            </thead>
-
-            <tbody>
-                @foreach ($clientes as $cliente)
+        <div class="table-responsive-sga">
+            <table class="table tabela-clientes">
+                <thead>
                     <tr>
-                        <td>{{ $cliente->telefone }}</td>
-                        <td>{{ $cliente->cpf }}</td>
-                        <td>{{ $cliente->nome }}</td>
-                        <td>{{ $cliente->endereco }}</td>
-                        <td>{{ $cliente->numero }}</td>
-                        <td>{{ $cliente->bairro }}</td>
-                        <td>{{ $cliente->cidade }}</td>
-                 
-                        <td>
-                            @if(auth()->user()->temPermissao('cliente_editar'))
-                                <a href="{{ route('clientes.edit', $cliente->id) }}" class="btn btn-editar">
-                                    Consultar/Alterar
-                                </a>
-                            @endif
-
-                            @if(auth()->user()->temPermissao('cliente_excluir'))
-                                <form action="{{ route('clientes.destroy', $cliente->id) }}" method="POST" style="display:inline;">
-                                    @csrf
-                                    @method('DELETE')
-
-                                    <button 
-                                        type="submit" 
-                                        class="btn btn-excluir"
-                                        onclick="return confirm('Tem certeza que deseja excluir este cliente?')"
-                                    >
-                                        Excluir
-                                    </button>
-                                </form>
-                            @endif
-                        </td>
+                        <th>Telefone</th>
+                        <th>CPF</th>
+                        <th>Nome</th>                  
+                        <th>Endereço</th>
+                        <th>Número</th>
+                        <th>Bairro</th>
+                        <th>Cidade</th>
+                        <th>Ações</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+
+                <tbody>
+                    @foreach ($clientes as $cliente)
+                        <tr>
+                            <td>{{ $cliente->telefone }}</td>
+                            <td>{{ $cliente->cpf }}</td>
+                            <td>{{ $cliente->nome }}</td>
+                            <td>{{ $cliente->endereco }}</td>
+                            <td>{{ $cliente->numero }}</td>
+                            <td>{{ $cliente->bairro }}</td>
+                            <td>{{ $cliente->cidade }}</td>
+                            <td class="acoes">
+                                @if(auth()->user()->temPermissao('cliente_editar'))
+                                    <a href="{{ route('clientes.edit', $cliente->id) }}" class="btn btn-editar">
+                                        Consultar/Alterar
+                                    </a>
+                                @endif
+
+                                @if(auth()->user()->temPermissao('cliente_excluir'))
+                                    <form action="{{ route('clientes.destroy', $cliente->id) }}" method="POST" class="form-excluir">
+                                        @csrf
+                                        @method('DELETE')
+
+                                        <button 
+                                            type="submit" 
+                                            class="btn btn-excluir"
+                                            onclick="return confirm('Tem certeza que deseja excluir este cliente?')"
+                                        >
+                                            Excluir
+                                        </button>
+                                    </form>
+                                @endif
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
 
     </div>
 
