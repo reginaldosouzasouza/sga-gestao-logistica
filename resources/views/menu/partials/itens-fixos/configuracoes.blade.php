2642ca56cc8325f20d7dc611d3ef122826aeacf3
REGINALDO SOUZA <reginaldo.aparecido.souzas@gmail.com>
2026-07-23 10:46:01 -0300

    Corrige acentuacao do menu de configuracoes

--- resources/views/menu/partials/itens-fixos/configuracoes.blade.php
+++ resources/views/menu/partials/itens-fixos/configuracoes.blade.php
@@ -1,31 +1,43 @@
-﻿@php
+@php
     $user = auth()->user();
 
     /*
-     * MASTER vÃª tudo.
-     * Os demais usuÃ¡rios obedecem ao perfil/permissÃµes.
+     * MASTER vê tudo.
+     * Os demais usuários obedecem ao perfil/permissões.
      */
     $isMaster = $user && strtoupper(trim($user->tipo ?? '')) === 'MASTER';
 
     /*
-     * UsuÃ¡rios
+     * Usuários
      */
-    $podeVerUsuarios = $isMaster || ($user && $user->temPermissao('usuario_visualizar'));
-    $podeCadastrarUsuarios = $isMaster || ($user && $user->temPermissao('usuario_cadastrar'));
+    $podeVerUsuarios =
+        $isMaster ||
+        ($user && $user->temPermissao('usuario_visualizar'));
+
+    $podeCadastrarUsuarios =
+        $isMaster ||
+        ($user && $user->temPermissao('usuario_cadastrar'));
 
     /*
-     * Perfis e permissÃµes
+     * Perfis e permissões
      */
-    $podeVerPerfis = $isMaster || ($user && $user->temPermissao('perfil_visualizar'));
-    $podeEditarPerfis = $isMaster || ($user && $user->temPermissao('perfil_editar'));
+    $podeVerPerfis =
+        $isMaster ||
+        ($user && $user->temPermissao('perfil_visualizar'));
+
+    $podeEditarPerfis =
+        $isMaster ||
+        ($user && $user->temPermissao('perfil_editar'));
 
     /*
      * Empresas
      */
-    $podeVerEmpresas = $isMaster || ($user && $user->temPermissao('empresa_visualizar'));
+    $podeVerEmpresas =
+        $isMaster ||
+        ($user && $user->temPermissao('empresa_visualizar'));
 
     /*
-     * ImportaÃ§Ã£o de Despesas
+     * Importação de Despesas
      */
     $podeVerImportacaoDespesas =
         $isMaster ||
@@ -42,121 +54,195 @@
         ($user && $user->temPermissao('backup_excluir'));
 
     /*
-     * EmissÃ£o de NF-e
+     * Emissão de NF-e
      */
-    $podeVerEmissaoNfe = $isMaster || ($user && $user->temPermissao('nfe_visualizar'));
+    $podeVerEmissaoNfe =
+        $isMaster ||
+        ($user && $user->temPermissao('nfe_visualizar'));
 @endphp
 
 
-{{-- UsuÃ¡rios --}}
+{{-- Usuários --}}
 @if($podeVerUsuarios || $podeCadastrarUsuarios || $isMaster)
-    <a href="#" class="menu-link" id="config-usuarios-link">
-        UsuÃ¡rios
-        <i class="bi bi-caret-right-fill" style="margin-left:auto"></i>
+    <a
+        href="#"
+        class="menu-link"
+        id="config-usuarios-link"
+    >
+        Usuários
+
+        <i
+            class="bi bi-caret-right-fill"
+            style="margin-left:auto"
+        ></i>
     </a>
 
-    <div class="dropdown-submenu" id="config-usuarios-submenu">
-
+    <div
+        class="dropdown-submenu"
+        id="config-usuarios-submenu"
+    >
         @if($podeCadastrarUsuarios)
-            <a href="{{ url('/usuarios/create') }}" target="_blank" rel="noopener noreferrer">
-                Cadastrar UsuÃ¡rio
+            <a
+                href="{{ url('/usuarios/create') }}"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Cadastrar Usuário
             </a>
         @endif
 
         @if($podeVerUsuarios)
-            <a href="{{ url('/usuarios') }}" target="_blank" rel="noopener noreferrer">
-                Gerenciar UsuÃ¡rios
+            <a
+                href="{{ url('/usuarios') }}"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Gerenciar Usuários
             </a>
         @endif
 
         @if($isMaster)
-            <a href="{{ route('usuarios.monitor-acessos') }}" target="_blank" rel="noopener noreferrer">
+            <a
+                href="{{ route('usuarios.monitor-acessos') }}"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
                 Monitor de Acessos
             </a>
         @endif
-
     </div>
 @endif
 
 
 {{-- Perfis do Sistema --}}
 @if($podeVerPerfis || $podeEditarPerfis)
-    <a href="#" class="menu-link" id="config-perfis-link">
+    <a
+        href="#"
+        class="menu-link"
+        id="config-perfis-link"
+    >
         Perfis do Sistema
-        <i class="bi bi-caret-right-fill" style="margin-left:auto"></i>
+
+        <i
+            class="bi bi-caret-right-fill"
+            style="margin-left:auto"
+        ></i>
     </a>
 
-    <div class="dropdown-submenu" id="config-perfis-submenu">
-
+    <div
+        class="dropdown-submenu"
+        id="config-perfis-submenu"
+    >
         @if($podeVerPerfis)
-            <a href="{{ url('/perfis') }}" target="_blank" rel="noopener noreferrer">
+            <a
+                href="{{ url('/perfis') }}"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
                 Listar Perfis
             </a>
         @endif
 
         @if($podeEditarPerfis)
-            <a href="{{ url('/perfis/1/permissoes') }}" target="_blank" rel="noopener noreferrer">
+            <a
+                href="{{ url('/perfis/1/permissoes') }}"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
                 Perfil - Administrador
             </a>
 
-            <a href="{{ url('/perfis/2/permissoes') }}" target="_blank" rel="noopener noreferrer">
+            <a
+                href="{{ url('/perfis/2/permissoes') }}"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
                 Perfil - Gerente
             </a>
 
-            <a href="{{ url('/perfis/3/permissoes') }}" target="_blank" rel="noopener noreferrer">
+            <a
+                href="{{ url('/perfis/3/permissoes') }}"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
                 Perfil - Operacional
             </a>
 
-            <a href="{{ url('/perfis/4/permissoes') }}" target="_blank" rel="noopener noreferrer">
+            <a
+                href="{{ url('/perfis/4/permissoes') }}"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
                 Perfil - Financeiro
             </a>
 
-            <a href="{{ route('perfis.administrador-salao') }}"
+            <a
+                href="{{ route('perfis.administrador-salao') }}"
                 target="_blank"
-                rel="noopener noreferrer">
-                Perfil - Administrador do SalÃ£o
+                rel="noopener noreferrer"
+            >
+                Perfil - Administrador do Salão
             </a>
-
         @endif
-
     </div>
 @endif
 
 
 {{-- Cadastro de Empresas --}}
 @if($podeVerEmpresas)
-    <a href="{{ url('/empresas') }}" target="_blank" rel="noopener noreferrer">
+    <a
+        href="{{ url('/empresas') }}"
+        target="_blank"
+        rel="noopener noreferrer"
+    >
         Cadastro de Empresas
     </a>
 @endif
 
-{{-- ImportaÃ§Ã£o de Clientes - Somente MASTER --}}
+
+{{-- Importação de Clientes - Somente MASTER --}}
 @if($isMaster)
-    <a href="{{ route('clientes.importar') }}" target="_blank" rel="noopener noreferrer">
-        ImportaÃ§Ã£o de Clientes
+    <a
+        href="{{ route('clientes.importar') }}"
+        target="_blank"
+        rel="noopener noreferrer"
+    >
+        Importação de Clientes
     </a>
 @endif
 
 
-{{-- ImportaÃ§Ã£o de Despesas --}}
+{{-- Importação de Despesas --}}
 @if($podeVerImportacaoDespesas)
-    <a href="{{ url('/financeiro/contas-a-pagar/importar-despesas') }}" target="_blank" rel="noopener noreferrer">
-        ImportaÃ§Ã£o de Despesas
+    <a
+        href="{{ url('/financeiro/contas-a-pagar/importar-despesas') }}"
+        target="_blank"
+        rel="noopener noreferrer"
+    >
+        Importação de Despesas
     </a>
 @endif
 
 
 {{-- Backup do Sistema --}}
 @if($podeVerBackup)
-    <a href="{{ route('backups.index') }}" target="_blank" rel="noopener noreferrer">
+    <a
+        href="{{ route('backups.index') }}"
+        target="_blank"
+        rel="noopener noreferrer"
+    >
         Backup do Sistema
     </a>
 @endif
 
 
-{{-- EmissÃ£o NF-e externa --}}
+{{-- Emissão de NF-e externa --}}
 @if($podeVerEmissaoNfe)
-    <a href="https://emissornfe.sebrae.com.br/" target="_blank" rel="noopener noreferrer">
-        EmissÃ£o de NF-e
+    <a
+        href="https://emissornfe.sebrae.com.br/"
+        target="_blank"
+        rel="noopener noreferrer"
+    >
+        Emissão de NF-e
     </a>
 @endif
